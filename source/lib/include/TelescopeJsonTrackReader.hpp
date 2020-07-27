75c078a8d16a5c9682ca8d1be0a2ad359f9543fa
Yi Liu <yi.liu@desy.de>
2020-07-27 13:42:22 +0200

    impplement reader

--- source/lib/include/TelescopeJsonTrackReader.hpp
+++ source/lib/include/TelescopeJsonTrackReader.hpp
@@ -3,8 +3,11 @@
 #include <Acts/Utilities/Logger.hpp>
 #include <memory>
 #include <string>
+#include <map>
 
 #include "ACTFW/Framework/IReader.hpp"
+#include "Acts/Surfaces/PlaneSurface.hpp"
+
 
 
 namespace rapidjson{
@@ -22,10 +25,13 @@ namespace Telescope{
 class TelescopeJsonTrackReader final : public FW::IReader {
  public:
   struct Config {
-    /// Where to read input files from.
     std::string inputDataFile;
-    /// Which particle collection to read into.
-    size_t maxSelectEventNum;
+    std::string outputTracks;
+    size_t oneEventMode;
+    size_t maxSelectDatapackNum;
+    double resX;
+    double resY;
+    std::map<size_t, std::shared_ptr<const Acts::Surface>> surfaces;
   };
 
   /// Construct the particle reader.
@@ -48,7 +54,7 @@ class TelescopeJsonTrackReader final : public FW::IReader {
   std::unique_ptr<const Acts::Logger> m_logger;
   std::unique_ptr<rapidjson::CrtAllocator> m_jsa;
   std::unique_ptr<rapidjson::GenericValue<rapidjson::UTF8<char>, rapidjson::CrtAllocator>>  m_js_selected_datapack_col;
-  
+
   const Acts::Logger& logger() const { return *m_logger; }
 };
 
--- source/lib/src/TelescopeJsonTrackReader.cpp
+++ source/lib/src/TelescopeJsonTrackReader.cpp
@@ -12,6 +12,9 @@
 #include "ACTFW/Framework/WhiteBoard.hpp"
 #include "ACTFW/Utilities/Paths.hpp"
 
+
+#include "PixelSourceLink.hpp"
+
 #include "myrapidjson.h"
 
 using JsonValue = rapidjson::GenericValue<rapidjson::UTF8<char>, rapidjson::CrtAllocator>;
@@ -24,8 +27,8 @@ Telescope::TelescopeJsonTrackReader::TelescopeJsonTrackReader(
 {
 
   std::string datafile_name = m_cfg.inputDataFile;
-  size_t n_datapack_select_opt = m_cfg.maxSelectEventNum;
- 
+  size_t n_datapack_select_opt = m_cfg.maxSelectDatapackNum;
+
   m_jsa.reset(new rapidjson::CrtAllocator);
   m_js_selected_datapack_col.reset(new JsonValue(rapidjson::kArrayType) );
   {
@@ -47,8 +50,8 @@ Telescope::TelescopeJsonTrackReader::TelescopeJsonTrackReader(
     }
 
     uint64_t processed_datapack_count = 0;
-    for(auto ev_it = doc.Begin(); ev_it != doc.End() && m_js_selected_datapack_col->GetArray().Size() < n_datapack_select_opt ; ev_it++ ){
-      auto &evpack = *ev_it;
+    for(auto datapack_it = doc.Begin(); datapack_it != doc.End() && m_js_selected_datapack_col->GetArray().Size() < n_datapack_select_opt ; datapack_it++ ){
+      auto &evpack = *datapack_it;
       processed_datapack_count ++;
       auto &frames = evpack["layers"];
       bool is_good_datapack = true;
@@ -62,12 +65,16 @@ Telescope::TelescopeJsonTrackReader::TelescopeJsonTrackReader(
       if(!is_good_datapack){
         continue;
       }
-      m_js_selected_datapack_col->PushBack(evpack, *m_jsa);
+      m_js_selected_datapack_col->PushBack(evpack, *m_jsa); // moved
     }
   }
-  
-  m_eventsRange = {0, m_js_selected_datapack_col->GetArray().Size()};
-  
+
+  if(cfg.oneEventMode){
+    m_eventsRange = {0, 1};
+  }
+  else{
+    m_eventsRange = {0, m_js_selected_datapack_col->GetArray().Size()};
+  }
 }
 
 std::string Telescope::TelescopeJsonTrackReader::name() const {
@@ -75,16 +82,49 @@ std::string Telescope::TelescopeJsonTrackReader::name() const {
 }
 
 std::pair<size_t, size_t> Telescope::TelescopeJsonTrackReader::availableEvents() const {
-
-  
   return m_eventsRange;
 }
 
 FW::ProcessCode Telescope::TelescopeJsonTrackReader::read(const FW::AlgorithmContext& ctx) {
 
-  // SimParticleContainer particles;
-  // particles.adopt_sequence(std::move(unordered));
-  // ctx.eventStore.add(m_cfg.outputParticles, std::move(particles));
+  Acts::BoundMatrix cov_hit = Acts::BoundMatrix::Zero();
+  cov_hit(0, 0) =m_cfg.resX*m_cfg.resX;
+  cov_hit(1, 1) =m_cfg.resY*m_cfg.resY;
 
+  if(m_cfg.oneEventMode){
+    std::vector<std::vector<Telescope::PixelSourceLink>> sourcelinkTracks;
+    sourcelinkTracks.reserve(m_js_selected_datapack_col->Size());
+    for(auto datapack_it = m_js_selected_datapack_col->Begin(); datapack_it != m_js_selected_datapack_col->End() ; datapack_it++ ){
+      std::vector<Telescope::PixelSourceLink> sourcelinks;
+      const auto &datapack = *datapack_it;
+      const auto &layers = datapack["layers"];
+      {
+        for(const auto& [i, s] : m_cfg.surfaces){
+          double x_hit = layers[i]["hit"][0]["pos"][0].GetDouble() - 0.02924*1024/2.0;
+          double y_hit = layers[i]["hit"][0]["pos"][1].GetDouble() - 0.02688*512/2.0;
+          Acts::Vector2D loc_hit;
+          loc_hit << x_hit, y_hit;
+          sourcelinks.emplace_back(*s, loc_hit, cov_hit);
+        }
+      }
+      sourcelinkTracks.push_back(std::move(sourcelinks));
+    }
+    ctx.eventStore.add(m_cfg.outputTracks, std::move(sourcelinkTracks));
+  }
+  else{
+    std::vector<Telescope::PixelSourceLink> sourcelinks;
+    const auto &datapack = (*m_js_selected_datapack_col)[ctx.eventNumber];
+    const auto &layers = datapack["layers"];
+    {
+      for(const auto& [i, s] : m_cfg.surfaces){
+        double x_hit = layers[i]["hit"][0]["pos"][0].GetDouble() - 0.02924*1024/2.0;
+        double y_hit = layers[i]["hit"][0]["pos"][1].GetDouble() - 0.02688*512/2.0;
+        Acts::Vector2D loc_hit;
+        loc_hit << x_hit, y_hit;
+        sourcelinks.emplace_back(*s, loc_hit, cov_hit);
+      }
+    }
+    ctx.eventStore.add(m_cfg.outputTracks, std::move(sourcelinks));
+  }
   return FW::ProcessCode::SUCCESS;
 }
