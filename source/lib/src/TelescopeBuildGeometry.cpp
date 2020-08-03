4f59d1ec2bc51885bd54441825e5d5517b648a92
Yi Liu <yi.liu@desy.de>
2020-08-03 13:25:49 +0200

    simplified build geo

--- source/lib/src/TelescopeBuildGeometry.cpp
+++ source/lib/src/TelescopeBuildGeometry.cpp
@@ -47,32 +47,6 @@ void Telescope::BuildGeometry(
                               const rapidjson::GenericValue<rapidjson::UTF8<>, rapidjson::CrtAllocator> &js_opt
                               ){
 
-  std::vector<Acts::Transform3D> transforms;
-  for(const auto& js_l: js_opt.GetArray()){
-    double cx = js_l["centerX"].GetDouble();
-    double cy = js_l["centerY"].GetDouble();
-    double cz = js_l["centerZ"].GetDouble();
-    double rx = js_l["rotX"].GetDouble();
-    double ry = js_l["rotY"].GetDouble();
-    double rz = js_l["rotZ"].GetDouble();
-    // Acts::Vector3D center3d;
-    Acts::Vector3D translation(cx,cy,cz);
-     // The rotation around global z axis
-      Acts::AngleAxis3D rotZ(rz,
-                             Acts::Vector3D::UnitZ());
-      // The rotation around global y axis
-      Acts::AngleAxis3D rotY(ry,
-                             Acts::Vector3D::UnitY());
-      // The rotation around global x axis
-      Acts::AngleAxis3D rotX(rx,
-                             Acts::Vector3D::UnitX());
-      Acts::Rotation3D rotation = rotZ * rotY * rotX;
-
-      const Acts::Transform3D transform =
-          Acts::Translation3D(translation) * rotation;
-    transforms.push_back(std::move(transform));
-  }
-
   // Boundaries of the surfaces (ALPIDE real SIZE: 29.941760 * 13.762560_mm*mm)
   const auto rBounds = std::make_shared<const Acts::RectangleBounds>
    (Acts::RectangleBounds(30_mm/2.0, 14_mm/2.0));
@@ -80,56 +54,76 @@ void Telescope::BuildGeometry(
   // Material of the surfaces
   const auto surfaceMaterial = std::make_shared<Acts::HomogeneousSurfaceMaterial>
     (Acts::MaterialProperties(95.7, 465.2, 28.03, 14., 2.32e-3, 80_um));
-  
+
   // Set translation vectors
-  for (unsigned int i = 0; i < transforms.size(); i++) {
-    auto trafo = std::make_shared<Acts::Transform3D>(transforms[i]);
+
+  for(const auto& js_l: js_opt.GetArray()){
+
+    // Acts::Vector3D center3d;
+    double cx = js_l["centerX"].GetDouble();
+    double cy = js_l["centerY"].GetDouble();
+    double cz = js_l["centerZ"].GetDouble();
+    Acts::Vector3D translation(cx,cy,cz);
+
+    //Acts::Rotation3D
+    double rx = js_l["rotX"].GetDouble();
+    double ry = js_l["rotY"].GetDouble();
+    double rz = js_l["rotZ"].GetDouble();
+    // The rotation around global z axis
+    Acts::AngleAxis3D rotZ(rz, Acts::Vector3D::UnitZ());
+    // The rotation around global y axis
+    Acts::AngleAxis3D rotY(ry, Acts::Vector3D::UnitY());
+    // The rotation around global x axis
+    Acts::AngleAxis3D rotX(rx, Acts::Vector3D::UnitX());
+    Acts::Rotation3D rotation = rotZ * rotY * rotX;
+
+    //Acts::Transform3D
+    auto trafo = std::make_shared<Acts::Transform3D>
+      (Acts::Translation3D(translation) * rotation);
+
     // Create the detector element
-    auto detElement = std::make_shared<Telescope::TelescopeDetectorElement>(trafo, rBounds, 1_um, surfaceMaterial);
+    auto detElement = std::make_shared<Telescope::TelescopeDetectorElement>
+      (trafo, rBounds, 1_um, surfaceMaterial);
     auto detSurface = detElement->surface().getSharedPtr();
     surface_col.push_back(detSurface);
     element_col.push_back(detElement);
-    
+
     std::unique_ptr<Acts::SurfaceArray> surArray(new Acts::SurfaceArray(detSurface));
     auto detLayer = Acts::PlaneLayer::create(trafo, rBounds, std::move(surArray), 1.5_cm);
     layer_col.push_back(detLayer);
-    
+
     auto mutableSurface = std::const_pointer_cast<Acts::Surface>(detSurface);
     mutableSurface->associateLayer(*detLayer);
   }
 
+
+
   // Build tracking volume
+  float tracker_halfX= 0.1_m;
+  float tracker_halfY= 0.1_m;
+  float tracker_halfZ= 1.2_m;
+  auto tracker_cuboid = std::make_shared<Acts::CuboidVolumeBounds>(tracker_halfX, tracker_halfY, tracker_halfZ);
 
-  Acts::LayerArrayCreator::Config lacConfig;
-  Acts::LayerArrayCreator layArrCreator(lacConfig);  
-  std::unique_ptr<const Acts::LayerArray> layArr(layArrCreator.layerArray(nominal_gctx,
-                                                                          layer_col,
-                                                                          -0.5_m - 1._mm, 0.5_m + 1._mm,
-                                                                          Acts::BinningType::arbitrary,
-                                                                          Acts::BinningValue::binZ));
-  
-  auto boundsVol = std::make_shared<const Acts::CuboidVolumeBounds>(0.1_m, 0.1_m,1.2_m);
-  auto trafoVol = std::make_shared<Acts::Transform3D>(Acts::Transform3D::Identity());
-  trafoVol->translation() = Acts::Vector3D(0., 0., 0.);
+  auto trafo_no_move = std::make_shared<Acts::Transform3D>(Acts::Transform3D::Identity());
+  trafo_no_move->translation() = Acts::Vector3D(0., 0., 0.);
+
+  auto layer_array_binned = Acts::LayerArrayCreator({}).
+    layerArray(nominal_gctx, layer_col,
+               - tracker_halfZ, tracker_halfZ,
+               Acts::BinningType::arbitrary,
+               Acts::BinningValue::binZ);
+
   auto trackVolume = Acts::TrackingVolume::create
-    (trafoVol, boundsVol, nullptr, std::move(layArr), nullptr, {}, "Tracker");
-  
+    (trafo_no_move, tracker_cuboid, nullptr, std::move(layer_array_binned), nullptr, {}, "Tracker");
+
+
+
   // Build world volume
-  std::vector<std::pair<Acts::TrackingVolumePtr, Acts::Vector3D>> tapVec;
-  tapVec.push_back(std::make_pair(trackVolume, Acts::Vector3D(0., 0., 0.)));
+  // assumming single tracker in the world
+  auto tracker_array_binned = Acts::TrackingVolumeArrayCreator({}).
+    trackingVolumeArray( nominal_gctx, { trackVolume }, Acts::BinningValue::binZ );
+  auto mtvpWorld = Acts::TrackingVolume::create
+    (trafo_no_move, tracker_cuboid, tracker_array_binned , "World");
 
-  Acts::BinningData binData(Acts::BinningOption::open, Acts::BinningValue::binZ,
-                            std::vector<float>{-0.6_m, 0.6_m});
-  std::unique_ptr<const Acts::BinUtility> bu(new Acts::BinUtility(binData));
-
-  std::shared_ptr<const Acts::TrackingVolumeArray> trVolArr(
-                                                            new Acts::BinnedArrayXD<Acts::TrackingVolumePtr>(tapVec, std::move(bu)));
-
-  auto trafoWorld = std::make_shared<Acts::Transform3D>(Acts::Transform3D::Identity());
-  trafoWorld->translation() = Acts::Vector3D(0., 0., 0.);
-  auto worldVol = std::make_shared<const Acts::CuboidVolumeBounds>(0.1_m, 0.1_m, 1.2_m);
-  Acts::MutableTrackingVolumePtr mtvpWorld(Acts::TrackingVolume::create
-                                           (trafoWorld, worldVol, trVolArr, "World"));
-  
   geo_world.reset(new Acts::TrackingGeometry(mtvpWorld));
 }
