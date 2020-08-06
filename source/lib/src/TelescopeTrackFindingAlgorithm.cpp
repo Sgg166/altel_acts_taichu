9f6ff47d9d15f4a053987cb69d60cb901cb00775
Yi Liu <yi.liu@desy.de>
2020-08-06 23:23:01 +0200

    merger fun and alg

--- source/lib/src/TelescopeTrackFindingAlgorithm.cpp
+++ source/lib/src/TelescopeTrackFindingAlgorithm.cpp
@@ -10,8 +10,31 @@
 
 #include <stdexcept>
 
+#include <boost/program_options.hpp>
+#include <iostream>
+#include <map>
+#include <random>
+
+#include "Acts/Fitter/GainMatrixSmoother.hpp"
+#include "Acts/Fitter/GainMatrixUpdater.hpp"
+#include "Acts/Geometry/GeometryID.hpp"
+#include "Acts/MagneticField/ConstantBField.hpp"
+#include "Acts/MagneticField/InterpolatedBFieldMap.hpp"
+#include "Acts/MagneticField/SharedBField.hpp"
+#include "Acts/Propagator/Navigator.hpp"
+#include "Acts/Propagator/Propagator.hpp"
+#include "Acts/Propagator/StraightLineStepper.hpp"
+#include "Acts/Surfaces/Surface.hpp"
+#include "Acts/Utilities/Helpers.hpp"
+#include "Acts/Utilities/ParameterDefinitions.hpp"
+
+
+
 #include "ACTFW/EventData/Track.hpp"
 #include "ACTFW/Framework/WhiteBoard.hpp"
+#include "ACTFW/Plugins/BField/ScalableBField.hpp"
+
+
 
 Telescope::TelescopeTrackFindingAlgorithm::TelescopeTrackFindingAlgorithm(
     Config cfg, Acts::Logging::Level level)
@@ -95,4 +118,64 @@ FW::ProcessCode Telescope::TelescopeTrackFindingAlgorithm::execute(
 
   ctx.eventStore.add(m_cfg.outputTrajectories, std::move(trajectories));
   return FW::ProcessCode::SUCCESS;
+}
+
+
+namespace {
+  template <typename TrackFinder>
+  struct TrackFinderFunctionImpl {
+    TrackFinder trackFinder;
+
+    TrackFinderFunctionImpl(TrackFinder&& f) : trackFinder(std::move(f)) {}
+
+    Telescope::TelescopeTrackFindingAlgorithm::TrackFinderResult operator()
+    (
+     const std::vector<Telescope::PixelSourceLink>& sourceLinks,
+     const FW::TrackParameters& initialParameters,
+     const Acts::CombinatorialKalmanFilterOptions<Acts::CKFSourceLinkSelector>&
+     options) const {
+      return trackFinder.findTracks(sourceLinks, initialParameters, options);
+    }
+  };
+}
+
+Telescope::TelescopeTrackFindingAlgorithm::TrackFinderFunction
+Telescope::TelescopeTrackFindingAlgorithm::makeTrackFinderFunction(
+    std::shared_ptr<const Acts::TrackingGeometry> trackingGeometry,
+    FW::Options::BFieldVariant magneticField, Acts::Logging::Level lvl) {
+  using Updater = Acts::GainMatrixUpdater;
+  using Smoother = Acts::GainMatrixSmoother;
+
+  // unpack the magnetic field variant and instantiate the corresponding track
+  // finder.
+  return std::visit(
+      [trackingGeometry, lvl](auto&& inputField) -> TrackFinderFunction {
+        // each entry in the variant is already a shared_ptr
+        // need ::element_type to get the real magnetic field type
+        using InputMagneticField =
+            typename std::decay_t<decltype(inputField)>::element_type;
+        using MagneticField = Acts::SharedBField<InputMagneticField>;
+        using Stepper = Acts::StraightLineStepper;
+        using Navigator = Acts::Navigator;
+        using Propagator = Acts::Propagator<Stepper, Navigator>;
+        using CKF =
+            Acts::CombinatorialKalmanFilter<Propagator, Updater, Smoother,
+                                            Acts::CKFSourceLinkSelector>;
+
+        // construct all components for the track finder
+        MagneticField field(std::move(inputField));
+        Stepper stepper;
+        Navigator navigator(trackingGeometry);
+        navigator.resolvePassive = false;
+        navigator.resolveMaterial = true;
+        navigator.resolveSensitive = true;
+        Propagator propagator(std::move(stepper), std::move(navigator));
+        CKF trackFinder(
+            std::move(propagator),
+            Acts::getDefaultLogger("CombinatorialKalmanFilter", lvl));
+
+        // build the track finder functions. owns the track finder object.
+        return TrackFinderFunctionImpl<CKF>(std::move(trackFinder));
+      },
+      std::move(magneticField));
 }
